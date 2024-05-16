c2866ed2585f9a45ee771fea65bf94570417ea99
KhoiNNguyen <121741308+KhoiNNguyen@users.noreply.github.com>
2024-05-16 13:30:02 +0700

    accountcss

--- my-app/src/Pages/Client/account/account.js
+++ my-app/src/Pages/Client/account/account.js
@@ -6,11 +6,12 @@ import { LiaFileInvoiceSolid } from "react-icons/lia";
 
 function Account() {
     return ( 
+        <div className='background-all'>
         <div className="Inner">
             <div className="header-account">
                 <h4>TÀI KHOẢN</h4>
             </div>
-            <div className='container'>
+            <div className='container-account'>
             <div className='left-account'>
                 <div className='info-account'>
                     <div className='avater'>
@@ -22,24 +23,28 @@ function Account() {
                     </div>
                 </div>
                 <div className='function-account'>
-                    <div className='myaccount'>
-                    <PiUserCircleLight /> <span>Tài khoản của tôi</span>
+                    <div className='func myaccount active'>
+                    <PiUserCircleLight className='icon-account' /> <span>Tài khoản của tôi</span>
                     </div>
-                    <div className='myoder'>
-                       <LiaFileInvoiceSolid /> <span>Đơn hàng của tôi</span>
+                    <div className='func myorder'>
+                       <LiaFileInvoiceSolid className='icon-account'/> <span>Đơn hàng của tôi</span>
                     </div>
-                    <div className='address'>
-                        <CiLocationOn /> <span>Địa chỉ của tôi</span>
+                    <div className='func address'>
+                        <CiLocationOn className='icon-account'/> <span>Địa chỉ của tôi</span>
                     </div>
-                    <div className='favorite'>
-                        <CiHeart /> <span>Danh sách yêu thích</span>
+                    <div className='func favorite'>
+                        <CiHeart className='icon-account'/> <span>Danh sách yêu thích</span>
                     </div>
                 </div>
             </div>
-            <div className='right-account'>
-
+            <div className='Left' style={{"background-color": "#ffffff"}}>
+                <div className='header-right'>
+                    <h1>Thông tin cá nhân</h1>
+                    <button>Sửa thông tin</button>
+                </div>
             </div>
             </div>
+        </div>
         </div>
      );
 }
